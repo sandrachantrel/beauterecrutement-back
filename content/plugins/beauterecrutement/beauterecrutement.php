db94b7e023419869fc04204db0d719e723ed80c8
sandrachantrel <sandrachantrel@outlook.com>
2020-10-02 14:52:01 +0200

    administrator capabilities

--- content/plugins/beauterecrutement/beauterecrutement.php
+++ content/plugins/beauterecrutement/beauterecrutement.php
@@ -12,6 +12,16 @@
 
  //Require indispensable pour le chargement du post-type dans le back office
  require plugin_dir_path(__FILE__) . 'post-types/offer.php';
+ require plugin_dir_path(__FILE__) . 'roles/administrator.php';
  require plugin_dir_path(__FILE__) . 'taxonomies/localisation.php';
  require plugin_dir_path(__FILE__) . 'taxonomies/type.php';
  require plugin_dir_path(__FILE__) . 'taxonomies/job.php';
+
+ 
+ register_activation_hook( __FILE__, function(){
+     add_administrator_capabilities();
+ });
+
+ register_deactivation_hook( __FILE__, function(){
+     remove_administrator_capabilities();
+ });
---
+++ content/plugins/beauterecrutement/roles/administrator.php
@@ -0,0 +1,72 @@
+<?php
+
+if ( ! function_exists( 'add_administrator_capabilities' )) {
+
+    function add_administrator_capabilities() {
+
+        // fonction get_role pour recuperer l'objet du role 
+        $administrator= get_role('administrator');
+
+       // je viens rajouter les capabilities sur le role administrateur 
+
+       // Offres d'emploi
+            $administrator->add_cap('edit_offer');
+			$administrator->add_cap('read_offer');
+			$administrator->add_cap('delete_offer');
+			$administrator->add_cap('edit_offers');
+			$administrator->add_cap('edit_others_offers');
+			$administrator->add_cap('delete_offers');
+			$administrator->add_cap('publish_offers');
+			$administrator->add_cap('read_private_offers');
+			$administrator->add_cap('delete_private_offers');
+			$administrator->add_cap('delete_published_offers');
+			$administrator->add_cap('delete_others_offers');
+			$administrator->add_cap('edit_private_offers');
+			$administrator->add_cap('edit_published_offers');
+            $administrator->add_cap('create_offers');
+            
+        // Localisations
+            $administrator->add_cap('manage_localisations');
+
+        // Types de contrat
+            $administrator->add_cap('manage_types');
+
+        // Métiers
+            $administrator->add_cap('manage_jobs');
+
+    }
+}
+
+if (! function_exists('remove_administrator_capabilities')) {
+    function remove_administrator_capabilities()
+    {
+        $administrator = get_role('administrator');
+
+        // je viens supprimer les capabilities sur le role administrateur 
+
+        // Offres d'emploi
+        $administrator->remove_cap('edit_offer');
+        $administrator->remove_cap('read_offer');
+        $administrator->remove_cap('delete_offer');
+        $administrator->remove_cap('edit_offers');
+        $administrator->remove_cap('edit_others_offers');
+        $administrator->remove_cap('delete_offers');
+        $administrator->remove_cap('publish_offers');
+        $administrator->remove_cap('read_private_offers');
+        $administrator->remove_cap('delete_private_offers');
+        $administrator->remove_cap('delete_published_offers');
+        $administrator->remove_cap('delete_others_offers');
+        $administrator->remove_cap('edit_private_offers');
+        $administrator->remove_cap('edit_published_offers');
+        $administrator->remove_cap('create_offers');
+        
+    // Localisations
+        $administrator->remove_cap('manage_localisations');
+
+    // Types de contrat
+        $administrator->remove_cap('manage_types');
+
+    // Métiers
+        $administrator->remove_cap('manage_jobs');
+    }
+}
