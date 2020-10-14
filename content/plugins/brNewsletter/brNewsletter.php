9dc51f3581139481daeb9bb4fc4c8827141f7dae
sandrachantrel <sandrachantrel@outlook.com>
2020-10-14 11:52:49 +0200

    plugin brNewsletter

---
+++ content/plugins/brNewsletter/brNewsletter.php
@@ -0,0 +1,12 @@
+<?php 
+/*
+Plugin Name: BrNewsletter
+Description: Génération automatique d'une popin qui propose de s'abonner à la Newsletter du site
+Author: Sandra C.
+Version: 1.0
+*/
+
+// Sécuriser le plugin
+if (!defined('WPINC')) {
+  exit;
+}
---
+++ content/plugins/brNewsletter/index.php
@@ -0,0 +1,4 @@
+<?php
+// Silence is golden 
+
+// Fichier qui est exécuté par défaut si on accède au dossier en HTTP. Ce fichier ne fait rien.
